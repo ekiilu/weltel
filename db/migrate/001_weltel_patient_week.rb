8fdc5c74b7cd3375dad4ddef9d3533f19403dea4
Chris Dion <chris@verticallabs.ca>
2012-04-25 16:04:13 -0700

    migration hack

--- db/migrate/001_weltel_patient_week.rb
+++ db/migrate/001_weltel_patient_week.rb
@@ -1,10 +1,12 @@
 migration 1, :weltel_patient_week do
 
   up do
-  	Weltel::Patient.all.each do |patient|
-  		patient.week = 17
-  		patient.save
-  	end
+	begin
+  		Weltel::Patient.all.each do |patient|
+  			patient.week = 17
+  			patient.save
+  		end
+	end
   end
 
   down do
