25fdc037dea301321d2d0bbd2adb172c0ee00c63
Chris Dion <chris@verticallabs.ca>
2012-04-26 11:17:56 -0700

    better migration

--- db/migrate/001_weltel_patient_week.rb
+++ db/migrate/001_weltel_patient_week.rb
@@ -2,7 +2,7 @@ migration 1, :weltel_patient_week do
 
   up do
   	Weltel::Patient.auto_upgrade!(:default)
-		Weltel::Patient.all(:week => nil).each do |patient|
+		Weltel::Patient.all(:week => 0).each do |patient|
 			patient.week = 17
 			patient.save
 		end
