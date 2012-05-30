8b40d7d0ecce85bbc744a1cd6b5e88e9a45cf26e
Paul Schuegraf <paul@verticallabs.ca>
2012-05-30 13:37:27 -0700

    bad condition check

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -12,7 +12,7 @@ module TableHelper
 
   def filter_table_header_tag(filter_key, filter_enum, &block)
     content = capture { yield }.strip
-    active = filter_key == @filter_key.to_sym && @filter_value
+    active = @filter_key && @filter_value && filter_key == @filter_key.to_sym
     enum_options = options_for_enum(filter_enum, active ?  @filter_value : nil) # only show current if filter is active
 
     content_tag(:th) do
