7ebc24a52d6aa6bbc99de18abc1c560f1b640ba4
Chris Dion <chris@verticallabs.ca>
2012-06-06 10:59:13 -0700

    fix message display bug in dashboard and scheduling work

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 set(:output, "/www/weltel/current/log/cron.log")
 
 every(5.minutes) do
-	command("/www/weltel/current/script/weltel_responses")
+	command("curl http://127.0.0.1:3000/weltel/task/receive_responses")
 end
 
 every(5.minutes) do
