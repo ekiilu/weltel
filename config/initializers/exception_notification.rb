6685ce5bd42979353bf1a10b9cdea8b75b88d368
Chris Dion <chris@verticallabs.ca>
2012-10-12 16:52:38 -0700

    disable exception notifications

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,15 +1,11 @@
-#-  -*- encoding : utf-8 -*- 
+#-  -*- encoding : utf-8 -*-
 #- This Source Code Form is subject to the terms of the Mozilla Public
 #- License, v. 2.0. If a copy of the MPL was not distributed with this
 #- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# -*- encoding : utf-8 -*-
-if Rails.env.production?
-	Weltel::Application.config.middleware.use ExceptionNotifier,
-		:email_prefix => "[Weltel] ",
-		:sender_address => %{"notifier" <site@verticallabs.ca>},
-		:exception_recipients => %w{support@verticallabs.ca}
-end
-
-
-
+#if Rails.env.production?
+#	Weltel::Application.config.middleware.use ExceptionNotifier,
+#		:email_prefix => "[Weltel] ",
+#		:sender_address => %{"notifier" <site@verticallabs.ca>},
+#		:exception_recipients => %w{support@verticallabs.ca}
+#end
