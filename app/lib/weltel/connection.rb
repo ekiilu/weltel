9ef73ce35c6967b73d8d9e4ef074010f5a941c0d
Chris Dion <chris@verticallabs.ca>
2012-10-11 21:39:29 -0700

    cleanup

--- app/lib/weltel/connection.rb
+++ app/lib/weltel/connection.rb
@@ -4,5 +4,15 @@ module Weltel
 			self.write
 			system('god restart mambo_gammu')
 		end
+
+    def self.save_config(name, hash)
+      hash.each do |k, v|
+        Weltel::Config.create!(:name => name.to_s, :key => k.to_s, :value => v.to_s)
+      end
+    end
+
+    def self.load_config(name)
+      RecursiveOpenStruct.new(Hash[Weltel::Config.where(:name => name.to_s).map {|item| [item.key.to_sym, item.value]}])
+    end
 	end
 end
