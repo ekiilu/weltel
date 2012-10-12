23904da339dfb0463a43cf2128a412d5b89d9a9e
Chris Dion <chris@verticallabs.ca>
2012-10-11 21:28:21 -0700

    update connection controller

--- app/controllers/weltel/connections_controller.rb
+++ app/controllers/weltel/connections_controller.rb
@@ -4,7 +4,7 @@ module Weltel
     respond_to :html
 
     def destroy
-      @connection = Connection.load_config(:current)
+      @connection = Adapters::Connection.load_config(:current)
       connection_class = @connection.type.constantize
       connection_class.reset
 
@@ -12,14 +12,14 @@ module Weltel
     end
 
     def edit
-      @connection = Connection.load_config(:current)
-      @available_devices = Connection.available_devices
+      @connection = Adapters::Connection.load_config(:current)
+      @available_devices = Adapters::Connection.available_devices
     end
 
     def update
-      params.select! {|k,v| Connection.valid_keys.include?(k.to_sym) }
-      Connection.save_config(:current, params.merge({:type => Adapters::Gammu::Connection.to_s}))
-      @connection = Connection.load_config(:current)
+      params.select! {|k,v| Adapters::Connection.valid_keys.include?(k.to_sym) }
+      Adapters::Connection.save_config(:current, params.merge({:type => Adapters::Gammu::Connection.to_s}))
+      @connection = Adapters::Connection.load_config(:current)
       connection_class = @connection.type.constantize
       connection_class.reset
 
