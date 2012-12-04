5bb2a2a1b83aa6d4c957f87a3058a040ae494508
Paul Schuegraf <paul@verticallabs.ca>
2012-12-03 22:36:42 -0800

    twilio adapter

--- app/lib/weltel/factory.rb
+++ app/lib/weltel/factory.rb
@@ -7,12 +7,12 @@ module Weltel
 	class Factory
 		#
 		def self.sender
-			Adapters::Twilio::Sender.new(AppConfig.twilio.phone_number)
+			Adapters::TwilioAdapter::Sender.new(AppConfig.twilio.phone_number)
 		end
 
 		#
 		def self.poller
-			Adapters::Twilio::Poller.new(AppConfig.twilio.phone_number)
+			Adapters::TwilioAdapter::Poller.new(AppConfig.twilio.phone_number)
 		end
 
 		#
